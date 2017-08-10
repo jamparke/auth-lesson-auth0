7051132bb4f8ec835f17df071e141764b814eea3
Andy Parker <andyparker84@gmail.com>
2017-08-10 10:58:33 -0500

    complete lesson

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
+// done with massive v3, not backwards compatible
 const express = require('express'),
-      session = require('express-session'),
-      bodyParser = require('body-parser'),
-      massive = require('massive'),
-      passport = require('passport'),
-      Auth0Strategy = require('passport-auth0'),
-      config = require('./config.js'),
-      cors = require('cors');
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  massive = require('massive'),
+  passport = require('passport'),
+  Auth0Strategy = require('passport-auth0'),
+  config = require('./config.js'),
+  cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(session({
-  resave: true, //Without this you get a constant warning about default values
-  saveUninitialized: true, //Without this you get a constant warning about default values
+  resave: true, // Without this you get a constant warning about default values
+  saveUninitialized: true, // Without this you get a constant warning about default values
   secret: 'keyboardcat'
 }))
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(express.static('./public'));
+app.use(express.static('./public'))
 
 
 
@@ -27,8 +28,8 @@ app.use(express.static('./public'));
 /////////////
 const massiveInstance = massive.connectSync({connectionString: 'postgres://localhost/sandbox'})
 
-app.set('db', massiveInstance);
-const db = app.get('db');
+app.set('db', massiveInstance)
+const db = app.get('db')
 
 // db.create_user(function(err, user) {
 //   if (err) console.log(err);
@@ -38,49 +39,49 @@ const db = app.get('db');
 
 
 passport.use(new Auth0Strategy({
-   domain:       config.auth0.domain,
-   clientID:     config.auth0.clientID,
-   clientSecret: config.auth0.clientSecret,
-   callbackURL:  '/auth/callback'
-  },
-  function(accessToken, refreshToken, extraParams, profile, done) {
-    //Find user in database
-    db.getUserByAuthId([profile.id], function(err, user) {
-      user = user[0];
-      if (!user) { //if there isn't one, we'll create one!
-        console.log('CREATING USER');
-        db.createUserByAuth([profile.displayName, profile.id], function(err, user) {
-          console.log('USER CREATED', userA);
-          return done(err, user[0]); // GOES TO SERIALIZE USER
+  domain: config.auth0.domain,
+  clientID: config.auth0.clientID,
+  clientSecret: config.auth0.clientSecret,
+  callbackURL: '/auth/callback'
+},
+function (accessToken, refreshToken, extraParams, profile, done) {
+  // Find user in database
+  db.getUserByAuthId([profile.id], function (err, user) {
+    user = user[0]
+      if (!user) { // if there isn't one, we'll create one!
+      console.log('CREATING USER')
+        db.createUserByAuth([profile.displayName, profile.id], function (err, user) {
+        console.log('USER CREATED', userA)
+          return done(err, user[0]) // GOES TO SERIALIZE USER
         })
-      } else { //when we find the user, return it
-        console.log('FOUND USER', user);
-        return done(err, user);
+    } else { // when we find the user, return it
+      console.log('FOUND USER', user)
+        return done(err, user)
       }
-    })
-  }
-));
+  })
+}
+))
 
 //THIS IS INVOKED ONE TIME TO SET THINGS UP
-passport.serializeUser(function(userA, done) {
-  console.log('serializing', userA);
-  var userB = userA;
+passport.serializeUser(function (userA, done) {
+  console.log('serializing', userA)
+  var userB = userA
   //Things you might do here :
    //Serialize just the id, get other information to add to session, 
-  done(null, userB); //PUTS 'USER' ON THE SESSION
-});
+  done(null, userB) //PUTS 'USER' ON THE SESSION
+})
 
 //USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
-passport.deserializeUser(function(userB, done) {
-  var userC = userC;
+passport.deserializeUser(function (userB, done) {
+  var userC = userC
   //Things you might do here :
     // Query the database with the user id, get other information to put on req.user
-  done(null, userC); //PUTS 'USER' ON REQ.USER
-});
+  done(null, userC) //PUTS 'USER' ON REQ.USER
+})
 
 
 
-app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth', passport.authenticate('auth0'))
 
 
 //**************************//
@@ -92,21 +93,21 @@ app.get('/auth', passport.authenticate('auth0'));
 // });
 
 app.get('/auth/callback',
-  passport.authenticate('auth0', {successRedirect: '/'}), function(req, res) {
-    res.status(200).send(req.user);
+  passport.authenticate('auth0', {successRedirect: '/'}), function (req, res) {
+    res.status(200).send(req.user)
 })
 
-app.get('/auth/me', function(req, res) {
-  if (!req.user) return res.sendStatus(404);
+app.get('/auth/me', function (req, res) {
+  if (!req.user) return res.sendStatus(404)
   //THIS IS WHATEVER VALUE WE GOT FROM userC variable above.
-  res.status(200).send(req.user);
+  res.status(200).send(req.user)
 })
 
-app.get('/auth/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
+app.get('/auth/logout', function (req, res) {
+  req.logout()
+  res.redirect('/')
 })
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Connected on 3000')
 })
